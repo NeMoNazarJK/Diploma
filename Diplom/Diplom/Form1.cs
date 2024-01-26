7a7d936a8349f81175e7b0467ba3f184e7d3ec5f
NeMoNazarJK <nazariidovhal10@gmail.com>
2024-01-26 13:47:03 +0200

    Generating keys

--- Diplom/Diplom/Form1.cs
+++ Diplom/Diplom/Form1.cs
@@ -13,30 +13,30 @@ namespace Diplom
             InitializeFormElements();
         }
 
-        private Button btnOpenImage;
+        private Button btnGeneratingkeys;
         private TextBox txtFilterSize;
 
         private void InitializeFormElements()
         {
             this.Size = new Size(1920, 1080);
             this.StartPosition = FormStartPosition.CenterScreen;
-            this.Text = "RSA";          
+            this.Text = "RSA";
 
-            btnOpenImage = new Button
+            btnGeneratingkeys = new Button
             {
-                Location = new Point(1000, 1000),
-                Size = new Size(150, 30),
-                Text = ""
+                Location = new Point(500, 10),
+                Size = new Size(180, 22),
+                Text = "Згенерувати пару ключів"
             };
 
             txtFilterSize = new TextBox
             {
                 Location = new Point(10, 10),
                 Size = new Size(400, 400),
-                Text = "3"
+                Text = ""
             };
 
-            Controls.Add(btnOpenImage);
+            Controls.Add(btnGeneratingkeys);
             Controls.Add(txtFilterSize);
 
         }
